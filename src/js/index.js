51423842d4600a6ac829c8d48f72d900d40414cb
Jhony Freitas <jhony00._@hotmail.com>
2022-09-01 23:01:04 -0300

    Adicionando script do arquivo js

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+/*
+    - Onchange verificar se o campo está sendo preenchido
+    - Se estiver de acordo colocar uma variáve de controle correto
+    - No onchange atualizar o cartão 
+    - Fazer verificação se o campo está vazio 
+    - Fazer verificaçõ dse o campo esta com o fomato certo 
+    - Se todas variáveis de controle estiverem corretas colocar página de continue 
+    - Botão de continue volta pro formulário e limpa ele
+*/
+
+const verificationField = document.querySelectorAll('.verification')
+const inputs = document.querySelectorAll('input')
+
