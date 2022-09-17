fc7ccbf3e869f894f90bdc2b4b49c7c0f7745751
Jhony Freitas <jhony00._@hotmail.com>
2022-09-16 21:31:48 -0300

    Finalizado estrutura, layout e interação

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,131 @@
+
 /*
-    - Onchange verificar se o campo está sendo preenchido
+    - Error blank puxando no botão
     - Se estiver de acordo colocar uma variáve de controle correto
-    - No onchange atualizar o cartão 
-    - Fazer verificação se o campo está vazio 
-    - Fazer verificaçõ dse o campo esta com o fomato certo 
+    - Juntar oque der
     - Se todas variáveis de controle estiverem corretas colocar página de continue 
-    - Botão de continue volta pro formulário e limpa ele
 */
 
-const verificationField = document.querySelectorAll('.verification')
-const inputs = document.querySelectorAll('input')
+const form = document.querySelector("form")
+const inputs = document.querySelectorAll("input")
+const inputsNumbers = [inputs[2], inputs[3], inputs[4]]
+const fields = document.querySelectorAll(".fields")
+const btnConfirm = document.getElementById("btn-confirm")
+const btnContinue = document.getElementById("btn-continue")
+const formComplete = document.getElementsByClassName("completed-state")[0]
+const formData = document.getElementsByClassName("form-data")[0]
 
+const cardHolder = document.getElementById("cardholder-name")
+const cardNumber = document.getElementById("card-number")
+const month = document.getElementById("date-month")
+const year = document.getElementById("date-year")
+const cvc = document.getElementById("cvc")
+
+let frontCardYear = document.getElementById("card-date-year")
+let frontCardMonth = document.getElementById("card-date-month")
+let frontCardNumber = document.getElementById("front-card-number")
+let frontCardName = document.getElementById("front-card-name")
+let backCardCvc = document.getElementById("card-cvc")
+
+updateCardStatus()
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault(); 
+})    
+
+inputs.forEach((input) =>{
+    input.addEventListener("input", (target)=>{
+        if (!input.value){
+            target.path[1].classList.add("error-blank")
+            input.classList.remove("activated")
+        }else{
+            target.path[1].classList.remove("error-blank") 
+            input.classList.add("activated")
+        }
+    })
+})
+
+function updateCardStatus() {
+    cardHolder.addEventListener("change", () => {
+        frontCardName.innerHTML = cardHolder.value
+    })
+
+    cardNumber.addEventListener("change", () => {
+        frontCardNumber.innerHTML = cardNumber.value
+    })
+
+    month.addEventListener("change", () => {
+        frontCardMonth.innerHTML = month.value
+    })
+
+    year.addEventListener("change", () => {
+        frontCardYear.innerHTML = year.value
+    })
+
+    cvc.addEventListener("change", () => {
+        backCardCvc.innerHTML = cvc.value
+    })
+}
+
+function clearCard() {
+    frontCardName.innerHTML = "Jane Appleseed"
+    frontCardNumber.innerHTML = "0000 0000 0000 0000"
+    frontCardMonth.innerHTML = "00"
+    frontCardYear.innerHTML = "00"
+    backCardCvc.innerHTML = "000"
+
+    btnConfirm.classList.add("disabled")
+
+    inputs.forEach((input) =>{
+        input.value = ""
+        input.classList.remove("activated")
+    })
+}
+
+cardNumber.addEventListener('keypress', ()=>{
+    let numberLength = cardNumber.value.length
+
+    if (numberLength === 4 || numberLength === 9 || numberLength === 14) {
+        cardNumber.value += ' '
+    }
+    
+})
+
+btnConfirm.addEventListener("click", ()=>{
+    formData.style.display = "none"
+    formComplete.style.display = "flex"
+})
+
+btnContinue.addEventListener("click", ()=>{
+    clearCard()
+    formComplete.style.display = "none"
+    formData.style.display = "block"
+})
+
+cardNumber.addEventListener("change", ()=>{
+    const regex = /[a-z]/
+    const numberTest = regex.test(cardNumber.value)
+    if (numberTest){
+        fields[1].classList.add("error-wrong")
+    }else{
+        fields[1].classList.remove("error-wrong")
+    }    
+})
+
+inputsNumbers.forEach((input) =>{
+    input.addEventListener("change", (target)=>{
+        if (isNaN(input.value)) {
+            target.path[1].classList.add("error-wrong")
+        }else{
+            target.path[1].classList.remove("error-wrong")
+        }
+    })
+})
+
+form.addEventListener("input", ()=>{
+    if (cardHolder.classList == "activated" && cardNumber.classList == "activated" && month.classList == "activated" && year.classList == "activated" && cvc.classList == "activated"){
+        btnConfirm.classList.remove("disabled")
+    }else{
+        btnConfirm.classList.add("disabled")
+    }
+})
