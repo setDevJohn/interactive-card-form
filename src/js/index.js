20ef099c5865806ef382880f86dff33f91f51607
Jhony Freitas <jhony00._@hotmail.com>
2023-07-05 20:23:36 -0300

    Arrumando problema de campos dos inputs

--- src/js/index.js
+++ src/js/index.js
@@ -7,32 +7,32 @@ const formData = document.getElementsByClassName("form-data")[0]
 const formComplete = document.getElementsByClassName("completed-state")[0]
 
 const form = {
-    name : document.getElementById("cardholder-name"),
-    number : document.getElementById("card-number"),
-    month : document.getElementById("date-month"),
-    year : document.getElementById("date-year"),
-    cvc : document.getElementById("cvc")
+    name: document.getElementById("cardholder-name"),
+    number: document.getElementById("card-number"),
+    month: document.getElementById("date-month"),
+    year: document.getElementById("date-year"),
+    cvc: document.getElementById("cvc")
 }
 const card = {
-    number : document.getElementById("front-card-number"),
-    name : document.getElementById("front-card-name"),
-    month : document.getElementById("card-date-month"),
-    year : document.getElementById("card-date-year"),
-    cvc : document.getElementById("card-cvc")
+    number: document.getElementById("front-card-number"),
+    name: document.getElementById("front-card-name"),
+    month: document.getElementById("card-date-month"),
+    year: document.getElementById("card-date-year"),
+    cvc: document.getElementById("card-cvc")
 }
 
 updateCardStatus()
 
-cardForm.addEventListener('submit', (event)=>{
-    event.preventDefault(); 
-})   
+cardForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+})
 
-btnConfirm.addEventListener("click", ()=>{
+btnConfirm.addEventListener("click", () => {
     formData.style.display = "none"
     formComplete.style.display = "flex"
 })
 
-btnContinue.addEventListener("click", ()=>{
+btnContinue.addEventListener("click", () => {
     clearCard()
     formComplete.style.display = "none"
     formData.style.display = "flex"
@@ -67,57 +67,61 @@ function clearCard() {
     card.year.innerHTML = "00"
     card.cvc.innerHTML = "000"
 
-    inputs.forEach((input) =>{
+    inputs.forEach((input) => {
         input.value = ""
         input.classList.remove("activated")
-        input.style.borderColor ="#c8c4c4"
+        input.style.borderColor = "#c8c4c4"
     })
 
     btnConfirm.classList.add("disabled")
 }
 
-inputs.forEach((input) =>{
-    input.addEventListener("input", (target)=>{
-        if (!input.value){
-            target.path[1].classList.add("error-blank")
+inputs.forEach((input) => {
+    input.addEventListener("input", (e) => {
+        const inputField = e.target.nextSibling.parentElement;
+        if (!input.value) {
+            inputField.classList.add("error-blank")
             input.classList.remove("activated")
-            input.style.borderColor ='#ff5252'
-        }else{
-            target.path[1].classList.remove("error-blank") 
+            input.style.borderColor = '#ff5252'
+        } else {
+            inputField.classList.remove("error-blank")
             input.classList.add("activated")
-            input.style.borderColor ='#6448fe'
+            input.style.borderColor = '#6448fe'
         }
     })
 })
 
-inputsNumbers.forEach((input) =>{
-    input.addEventListener("change", (target)=>{
+inputsNumbers.forEach((input) => {
+    input.addEventListener("change", (e) => {
+
+        const inputField = e.target.nextSibling.parentElement;
+
         if (isNaN(input.value)) {
-            target.path[1].classList.add("error-wrong")
+            inputField.classList.add("error-wrong")
             input.classList.remove("activated")
-            input.style.borderColor ='#ff5252'
-        }else{
-            target.path[1].classList.remove("error-wrong")
+            input.style.borderColor = '#ff5252'
+        } else {
+            inputField.classList.remove("error-wrong")
             input.classList.add("activated")
         }
     })
 })
 
-form.number.addEventListener("change", ()=>{
+form.number.addEventListener("change", () => {
     const cardNumberField = document.querySelectorAll(".fields")[1]
     const regex = /[a-z]/
     const numberTest = regex.test(form.number.value)
-    if (numberTest){
+    if (numberTest) {
         cardNumberField.classList.add("error-wrong")
         form.number.classList.remove("activated")
-        form.number.style.borderColor ='#ff5252'
-    }else{
+        form.number.style.borderColor = '#ff5252'
+    } else {
         cardNumberField.classList.remove("error-wrong")
         form.number.classList.add("activated")
-    }    
+    }
 })
 
-form.number.addEventListener('keypress', ()=>{
+form.number.addEventListener('keypress', () => {
     let numberLength = form.number.value.length
 
     if (numberLength === 4 || numberLength === 9 || numberLength === 14) {
@@ -125,10 +129,10 @@ form.number.addEventListener('keypress', ()=>{
     }
 })
 
-cardForm.addEventListener("change", ()=>{
-    if (form.name.classList == "activated" && form.number.classList == "activated" && form.month.classList == "activated" && form.year.classList == "activated" && form.cvc.classList == "activated"){
+cardForm.addEventListener("change", () => {
+    if (form.name.classList == "activated" && form.number.classList == "activated" && form.month.classList == "activated" && form.year.classList == "activated" && form.cvc.classList == "activated") {
         btnConfirm.classList.remove("disabled")
-    }else{
+    } else {
         btnConfirm.classList.add("disabled")
     }
 })
